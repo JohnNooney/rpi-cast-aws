a9ff855cc34966e26798139b4721f3ffb55e3f1d
John Nooney <1803534@uad.ac.uk>
2022-01-13 15:37:19 +0000

    Add AWS DynamoDB get and put

--- aws/main.py
+++ aws/main.py
@@ -1,16 +1,38 @@
-# This is a sample Python script.
+import boto3
+from botocore.config import Config
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def dynamo_get_table(name):
+    # Get the service resource.
+    dynamodb = boto3.resource('dynamodb')
 
+    # Instantiate a table resource object without actually
+    # creating a DynamoDB table. Note that the attributes of this table
+    # are lazy-loaded: a request is not made nor are the attribute
+    # values populated until the attributes
+    # on the table resource are accessed or its load() method is called.
+    table = dynamodb.Table(name)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    # Print out some data about the table.
+    # This will cause a request to be made to DynamoDB and its attribute
+    # values will be set based on the response.
+    print(table.creation_date_time)
+    return table
 
+# put item as specificed json format
+def dynamo_put(table, data):
+    table.put_item(Item=data)
+
+def s3_get_buckets():
+    s3 = boto3.resource('s3')
+
+    for bucket in s3.buckets.all():
+        print(bucket.name)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    table = dynamo_get_table('test-ddb')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    #s3_get_buckets()
+
+
+
