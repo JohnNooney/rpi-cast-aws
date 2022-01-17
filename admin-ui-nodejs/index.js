0261478bfb45e38d8ebe7dc2423fff0231d30d0c
John Nooney <1803534@uad.ac.uk>
2022-01-17 22:38:05 +0000

    Add NodeJS app
    replace react

---
+++ admin-ui-nodejs/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = process.env.PORT || "8000";
+
+// app.get("/", (req, res) => {
+//     res.status(200).send("WHATABYTE: Food For Devs");
+//   });
+
+app.listen(port, () => {
+console.log(`Listening to requests on http://localhost:${port}`);
+});
+
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "pug");
+
+// app.get("/", (req, res) => {
+//     res.render("index", { title: "Home" });
+//   });
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+  });
