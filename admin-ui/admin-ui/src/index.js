88160f075ed0f67fdf5ba892a0051f0a21327910
John Nooney <1803534@uad.ac.uk>
2022-01-17 14:49:46 +0000

    Add AWS setup

--- admin-ui/admin-ui/src/index.js
+++ admin-ui/admin-ui/src/index.js
@@ -3,6 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//import AWS from 'aws-sdk';
+import * as AWS from 'aws-sdk'
+
+
+//Initialize the Amazon Cognito credentials provider
+AWS.config.region = 'us-east-1'; // Region
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+});
+
+AWS.config.credentials.get(function(err) {
+	if (err) {
+		console.log("Error: "+err);
+		return;
+	}
+	console.log("Cognito Identity Id: " + AWS.config.credentials.identityId);
+
+	// Other service clients will automatically use the Cognito Credentials provider
+	// configured in the JavaScript SDK.
+	var cognitoSyncClient = new AWS.CognitoSync();
+	cognitoSyncClient.listDatasets({
+		IdentityId: AWS.config.credentials.identityId,
+		IdentityPoolId: AWS.config.credentials.identityPoolId
+	}, function(err, data) {
+		if ( !err ) {
+			console.log(JSON.stringify(data));
+		}
+	});
+});
+
+// AWS.config.loadFromPath('./libs/config.json');
+
 
 ReactDOM.render(
   <React.StrictMode>
