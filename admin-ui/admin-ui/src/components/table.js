9091515c2fb8f98fea42b49f63a38c528882aaf6
John Nooney <1803534@uad.ac.uk>
2022-01-17 14:47:48 +0000

    Add AWS DyanmoDB Requests

--- admin-ui/admin-ui/src/components/table.js
+++ admin-ui/admin-ui/src/components/table.js
@@ -1,7 +1,48 @@
 import {Table, Space} from 'antd';
+
+// Create service client module using ES6 syntax.
+import {ScanCommand } from "@aws-sdk/client-dynamodb";
+import { ddbClient } from '../libs/ddbClient';
+
 const { Column } = Table;
 
+
+
+
+// Set the parameters.
+export const params = {
+// Specify which items in the results are returned.
+// FilterExpression: "Subtitle = :topic AND Season = :s AND Episode = :e",
+// // Define the expression attribute value, which are substitutes for the values you want to compare.
+// ExpressionAttributeValues: {
+//     ":topic": { S: "SubTitle2" },
+//     ":s": { N: "1" },
+//     ":e": { N: "2" },
+// },
+// Set the projection expression, which the the attributes that you want.
+ProjectionExpression: "log-hash, log-sort, session, status, duration, fault, restarts",
+TableName: "dbb-test",
+};
+
+export const run = async () => {
+    try {
+        const data = await ddbClient.send(new ScanCommand(params));
+        
+        data.Items.forEach(function (element, index, array) {
+            console.log(element);
+        });
+        console.log("All data: ", data);
+        return data;
+    } catch (err) {
+        console.log("Error", err);
+    }
+}
+
+
 function DataTable() {
+    var data =  run();
+    console.log("All data in test: ", data);
+
     return (
         <Table dataSource={json_data} id="admin-table">
             <Column title="Timestamp" dataIndex="log-hash" key="log-hash" />
